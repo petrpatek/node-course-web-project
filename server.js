9a875be63d56a963499f1741c3298c2db59b0508
Petr Pátek <petrpatekml@gmail.com>
2017-03-05 16:05:53 +0100

    First version of server

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs= require('hbs')
+var app= express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req,res, next) => {
+    var now = new Date().toString()
+    console.log(now)
+next()
+
+})
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+
+})
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+    })
+app.get('/', (req, res) => {
+    //res.send( '<h1>Hello Express!</h1>')
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+    welcomeMessege: 'Welcome on our site',
+})
+})
+app.get('/about', ( req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Us',
+})
+})
+app.get('/bad', (req,res) => {
+    res.send( {
+        error: "Error"
+    })
+    })
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+});
